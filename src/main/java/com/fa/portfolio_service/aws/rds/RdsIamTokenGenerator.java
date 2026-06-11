1684567e0465ff1f7b8067257798ede4ee871160
sandhya <sandhya.machani47@gmail.com>
2026-06-11 21:30:20 +0200

    Set up DB connectivity using Secrets Manager and IAM authentication
    - Added DB config to Secrets Manager
    - Retrieved config and generated IAM auth token
    - Connected to DB using AWS IAM database authentication rather password
    - Verified successful database access

--- src/main/java/com/fa/portfolio_service/aws/rds/RdsIamTokenGenerator.java
+++ src/main/java/com/fa/portfolio_service/aws/rds/RdsIamTokenGenerator.java
@@ -1,5 +1,7 @@
 package com.fa.portfolio_service.aws.rds;
 
+import com.fa.portfolio_service.aws.secrets.SecretConfig;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +13,24 @@ import software.amazon.awssdk.services.rds.RdsUtilities;
 import javax.sql.DataSource;
 @Configuration
 public class RdsIamTokenGenerator {
-    private static final String DB_HOST =
-            "database-1.c7me8euy81hb.eu-north-1.rds.amazonaws.com";
 
-    private static final String DB_NAME = "finadvdb";
-    private static  final String DB_USER_NAME = "iam_user_1";
-    private static final String DB_URL =
-            "jdbc:mysql://" + DB_HOST + ":3306/" + DB_NAME+"?sslMode=REQUIRED";
+//    private static final String DB_HOST =
+//            "database-1.c7me8euy81hb.eu-north-1.rds.amazonaws.com";
+//    private static final String DB_NAME = "finadvdb";
+//    private static  final String DB_USER_NAME = "iam_user_1";
+//    private static final String DB_URL =
+//            "jdbc:mysql://" + DB_HOST + ":3306/" + DB_NAME+"?sslMode=REQUIRED";
+//    private static final String DB_PORT = "";
 
-    public static  String generateTocken(){
-        System.out.println(DB_URL);
-        var credentials = AwsBasicCredentials.create(
-        "ACCESS_KET","SECRET");
+    private  String DB_HOST;
+    private  String DB_NAME ;
+    private String DB_USER_NAME ;
+    private  int DB_PORT ;
+
+    public   String generateTocken(){
+
+        var credentials = AwsBasicCredentials.create("","");
+
         var rdsClient =
                 software.amazon.awssdk.services.rds.RdsClient.builder()
                         .region(Region.EU_NORTH_1)
@@ -38,10 +46,24 @@ public class RdsIamTokenGenerator {
                         .username(DB_USER_NAME));
     }
 
+    public void assign_db_details(){
+        JsonNode secret = null;
+        try {
+            secret =  SecretConfig.getDbSecret();
+            DB_HOST = secret.get("host").asText();
+            DB_PORT = secret.get("port").asInt();
+            DB_USER_NAME = secret.get("username").asText();
+            DB_NAME = secret.get("dbname").asText();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Bean
     public DataSource dataSource() {
-
+        assign_db_details();
+        String DB_URL = "jdbc:mysql://" + DB_HOST + ":3306/" + DB_NAME ;
+        System.out.println(DB_URL);
         String token = generateTocken();
         System.out.println("Token length = " + token.length());
         HikariDataSource ds = new HikariDataSource();
@@ -50,6 +72,7 @@ public class RdsIamTokenGenerator {
         ds.setPassword(token);
 
         return ds;
+       // +"?sslMode=REQUIRED
     }
 }
 
--- src/main/resources/README.md
+++ src/main/resources/README.md
@@ -41,3 +41,22 @@ bin/kafka-topics.sh --create --topic trade-requests --bootstrap-server localhost
 bin/kafka-topics.sh --create --topic  trade-executed --bootstrap-server localhost:9092 --partitions 3 —replication-factor 1
 
 bin/kafka-topics.sh --create --topic trade-failed --bootstrap-server localhost:9092 --partitions 3 —replication-factor 1
+
+
+CREATE USER 'iam_user_1' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+
+GRANT ALL PRIVILEGES ON finadvdb.* TO 'iam_user_1';
+
+FLUSH PRIVILEGES;
+
+SELECT user, plugin
+FROM mysql.user
+
+SELECT user, host, plugin
+FROM mysql.user
+WHERE user IN ('admin','iam_user_1','app_user');
+
+
+CREATE USER 'app_user'@'%' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT ALL PRIVILEGES ON finadvdb.* TO 'app_user'@'%';
+FLUSH PRIVILEGES;
